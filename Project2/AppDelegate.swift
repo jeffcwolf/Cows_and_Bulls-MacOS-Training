31ce3b4a04888ab957dea9305668a48edeba3249
Jeff Wolf <jeffcwolf@gmail.com>
2020-09-08 20:24:46 +0200

    Initial Commit

---
+++ Project2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project2
+//
+//  Created by Jeffrey Wolf on 08.09.20.
+//  Copyright © 2020 Jeffrey Wolf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
