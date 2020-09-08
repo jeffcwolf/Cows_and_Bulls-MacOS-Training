726d17be1607b7b9941ab5a8ee5234df8d6803d2
Jeff Wolf <jeffcwolf@gmail.com>
2020-09-08 20:51:45 +0200

    got initial functions set up

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -8,20 +8,36 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet var tableView: NSTableView!
+    @IBOutlet var guess: NSTextField!
+    
+    var answer = ""
+    var guesses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func submitGuess(_ sender: NSButton) {
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
 
+    func result(for guess: String) -> String {
+        return "Result"
+    }
 
 }
 
